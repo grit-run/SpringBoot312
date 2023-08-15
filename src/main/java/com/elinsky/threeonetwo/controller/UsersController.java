6061655c2cdded722718de4d0e43a38e89e644c4
Elinsky <dmitry.ilinsky@gmail.com>
2023-08-15 14:50:33 +0300

    small changes

--- src/main/java/com/elinsky/threeonetwo/controller/UsersController.java
+++ src/main/java/com/elinsky/threeonetwo/controller/UsersController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
-//@RequestMapping(path = "/")
 public class UsersController {
     public final UserServiceImpl userServiceImpl;
 
