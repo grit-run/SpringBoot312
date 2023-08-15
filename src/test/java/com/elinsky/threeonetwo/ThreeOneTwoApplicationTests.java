bdd0f9904cc06ab9204bedddc324193ea222eb6a
Elinsky <dmitry.ilinsky@gmail.com>
2023-08-15 18:08:49 +0300

    small changes

---
+++ src/test/java/com/elinsky/threeonetwo/ThreeOneTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elinsky.threeonetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeOneTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
